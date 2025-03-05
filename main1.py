dccf9128fd8ff5c5de101aa485520a03754a569a
imyapolzevotelya <islomjonov2008@gmail.com>
2025-03-05 17:10:59 +0500

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,7 @@
+name = 'Islom Inomovich Olimov'
+print (name)
+age = 15
+print (age)
+adress = 'Sergeli, Mirza Xaydar, 20'
+print (adress)
+print ( "Республика Узбекистан обрела независимость 1 сентября")
